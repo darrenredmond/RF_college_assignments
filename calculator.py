a99160fd1baa4739fe89c99695fc3a296d70c77e
tur8o <rpdf@me.com>
2017-04-06 19:31:10 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,52 @@
+## -*- coding: utf-8 -*-
+#"""
+#Created on Tue Mar 21 10:31:27 2017
+#
+#@author: robert.farrell
+#"""
+#import math
+#
+#class CalcOps(object):
+#    
+#    def __init__(self):
+#        self.add = ''
+#    
+def add(first, second):
+    #addition function
+    return first + second
+#    
+#    def subtract(self, first, second):
+#        #subtraction function
+#        return first - second
+#        
+#    def multiply(self, first, second):
+#        #multiply function
+#        return first * second
+#    
+#    def divide(self, first, second):
+#        #divide function (can't divide by zero, return error)
+#        if second == 0:
+#            return 'Error!'
+#        return first / float(second)
+#    
+#    def exponent(self, first, second):
+#        #power of function
+#        return first**second
+#    
+#    def square(self, first):
+#        #cube function
+#        return first * first
+#    
+#    def cube(self, first):
+#        #cube function
+#        return first ** 3
+#    
+#    def square_root(self, first):
+#        #square root function
+#        return math.sqrt(first)
+#        
+#    def percentage(self, first):
+#        #converts number to percentage
+#        return str(first * 100) + '%'
+#
+#mycalc = CalcOps()    
